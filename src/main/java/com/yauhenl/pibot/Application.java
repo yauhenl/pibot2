5e8f07c2bebb9e3f974e9d2fb3c47c2d790f8558
yauhenl <yauhen.lisimau@gmail.com>
2017-10-28 22:20:21 +0300

    add comment

--- src/main/java/com/yauhenl/pibot/Application.java
+++ src/main/java/com/yauhenl/pibot/Application.java
@@ -7,6 +7,7 @@ import org.glassfish.jersey.server.ResourceConfig;
 import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
 
+// run with --add-modules java.activation,java.xml.bind
 public class Application {
     public static void main(String[] args) {
         URI baseUri = UriBuilder.fromUri("http://localhost/").port(8080).build();
